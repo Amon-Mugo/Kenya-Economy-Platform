a8a5b70db68bda2b0b294ca5e4e8150d6336608c
Amon-Mugo <amonkariuki325@gmail.com>
2026-06-10 06:00:56 +0300

    refactor: declare raw_forex as dbt source, remove sources.yml, fix freshness config

--- kenya_econ/models/staging/stg_forex.sql
+++ kenya_econ/models/staging/stg_forex.sql
@@ -1,15 +1,17 @@
-with source as(
-    select*from{{source('kenya_econ','raw_forex')}}
+with source as (
+    select * from {{ source('kenya_econ', 'raw_forex') }}
 ),
 renamed as (
-    select 
+    select
         date,
         usd_kes,
         eur_kes,
         gbp_kes,
         ingested_at
     from source
-    where date is not null and usd_kes is not null and eur_kes is not null and gbp_kes is not null
-
+    where date is not null
+      and usd_kes is not null
+      and eur_kes is not null
+      and gbp_kes is not null
 )
 select * from renamed
